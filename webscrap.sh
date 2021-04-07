db606e374685e40a00c91800fb7bb39f2338200e
krishpranav <krisna.pranav@gmail.com>
2021-04-07 07:04:02 -0400

    adding files

--- webscrap.sh
+++ webscrap.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+Black="\e[1;90m"
+Red="\e[1;91m"
+Green="\e[1;92m"
+Yellow="\e[1;93m"
+Blue="\e[1;94m"
+Purple="\e[1;95m"
+White="\e[1;97m"
+clear
+
+banner(){
+    figlet WEBSCRAPE | lolcat
+}
